9ab9bd1e9fefc25dc2cc2b15db0d75e519e17205
Vitaly Sender <vitaly@twobulls.com>
2019-03-12 15:15:55 -0400

    Updated to Swift 4/Xcode 10
    - Updated Carthage dependencies.
    - Fixed syntax errors.

--- Sources/JsonPropertyLoader.swift
+++ Sources/JsonPropertyLoader.swift
@@ -43,11 +43,11 @@ final public class JsonPropertyLoader {
     fileprivate func stringWithoutComments(_ str: String) -> String {
         let pattern = "(([\"'])(?:\\\\\\2|.)*?\\2)|(\\/\\/[^\\n\\r]*(?:[\\n\\r]+|$)|(\\/\\*(?:(?!\\*\\/).|[\\n\\r])*\\*\\/))"
         let expression = try! NSRegularExpression(pattern: pattern, options: .anchorsMatchLines)
-        let matches = expression.matches(in: str, options: [], range: NSRange(location: 0, length: str.characters.count))
+        let matches = expression.matches(in: str, options: [], range: NSRange(location: 0, length: str.count))
         
         let ret = NSMutableString(string: str)
         for match in matches.reversed() {
-            let character = String(str[str.characters.index(str.startIndex, offsetBy: match.range.location)])
+            let character = String(str[str.index(str.startIndex, offsetBy: match.range.location)])
             if character != "\'" && character != "\"" {
                 ret.replaceCharacters(in: match.range, with: "")
             }
